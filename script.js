85b93c8081a81d38b80f25f510075e235b344fd2
Peon1089 <145878611+Peon089@users.noreply.github.com>
2023-12-05 06:04:25 -0300

    d

--- script.js
+++ script.js
@@ -1,27 +1,3 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.0/firebase-app.js";
-import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.7.0/firebase-analytics.js";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyCHKZAX60gMIZCKNX91Bm6zT9JwhxQck3o",
-  authDomain: "caixa-de-texto.firebaseapp.com",
-  projectId: "caixa-de-texto",
-  storageBucket: "caixa-de-texto.appspot.com",
-  messagingSenderId: "579508003626",
-  appId: "1:579508003626:web:939477699a75f805603453",
-  measurementId: "G-DM6RZDMWKE"
-};
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
-firebase.initializeApp(config);
-const db = firebase.firestore();
-db.settings({ timestampsInSnapshots: true});
 document.addEventListener('DOMContentLoaded', function() {
     const display = document.getElementById('display');
     const buttons = document.getElementById('buttons');
